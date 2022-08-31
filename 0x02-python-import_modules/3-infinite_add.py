c6d21ecfce275c1823ef814a91bbfdd6e27b0c50
mulex12 <mulex.john@gmail.com>
2022-08-31 05:23:29 -0700

    4rth

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     add = 0
-    for s in argv[1:]:
-        add += int(s)
-        print("{:d}".format(add))
+    for i in argv[1:]:
+        add += int(i)
+    print("{:d}".format(add))
