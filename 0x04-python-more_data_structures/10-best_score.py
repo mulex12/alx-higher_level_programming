52937e670b0969e8ba8fec0b026fa7a7800fb803
mulex12 <mulex.john@gmail.com>
2022-08-31 10:37:13 -0700

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(m_dict):
+    if m_dict and len(m_dict):
+        max = list(m_dict.keys())[0]
+        for k in m_dict:
+            if m_dict[k] > m_dict[max]:
+                max = k
+            return max
+        return None
